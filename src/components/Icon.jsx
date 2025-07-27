6a8f538151e3f2b8e6945d26b1f88a17fa63d70e
pippsza <bastahov18@gmail.com>
2025-07-27 17:04:27 +0300

    icon fix

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,6 +1,6 @@
 const Icon = ({ name, className = "w-4 h-4" }) => (
   <svg className={className}>
-    <use href={`/src/assets/symbol-defs.svg#icon-${name}`} />
+    <use href={`/symbol-defs.svg#icon-${name}`} />
   </svg>
 );
 
